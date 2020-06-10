0c6d73d6a16e22ce05dcf5f335bb88e2ff02c408
Thiago V. Lemes <lemes_vilarinho@yahoo.com.br>
2020-06-10 15:24:40 -0300

    Consulta simples com query methods

--- src/main/java/com/thiagolemes/workshopmongodb/repositories/PostRepository.java
+++ src/main/java/com/thiagolemes/workshopmongodb/repositories/PostRepository.java
@@ -4,6 +4,11 @@ import com.thiagolemes.workshopmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text); // Busca por uma string no titulo do post. Sempre deve ser findBynome_a_ser_buscadoContaining
 }
--- src/main/java/com/thiagolemes/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/thiagolemes/workshopmongodb/resources/PostResource.java
@@ -1,18 +1,14 @@
 package com.thiagolemes.workshopmongodb.resources;
 
 import com.thiagolemes.workshopmongodb.domain.Post;
-import com.thiagolemes.workshopmongodb.domain.User;
-import com.thiagolemes.workshopmongodb.dto.UserDTO;
+import com.thiagolemes.workshopmongodb.resources.util.URL;
 import com.thiagolemes.workshopmongodb.services.PostService;
-import com.thiagolemes.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -25,6 +21,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) throws UnsupportedEncodingException {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
