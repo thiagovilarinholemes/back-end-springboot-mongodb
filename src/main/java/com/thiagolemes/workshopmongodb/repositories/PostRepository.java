6211bb78d8ee50b5ff6c176b7242fcf4f9537930
Thiago V. Lemes <lemes_vilarinho@yahoo.com.br>
2020-06-10 15:47:46 -0300

    Consulta simples com @Query

--- src/main/java/com/thiagolemes/workshopmongodb/repositories/PostRepository.java
+++ src/main/java/com/thiagolemes/workshopmongodb/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package com.thiagolemes.workshopmongodb.repositories;
 
 import com.thiagolemes.workshopmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,10 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    // Consulta por metodo
     List<Post> findByTitleContainingIgnoreCase(String text); // Busca por uma string no titulo do post. Sempre deve ser findBynome_a_ser_buscadoContaining
+
+    // Consulta por JSON
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }") // ?0 primeiro parametro do metodo abaixo, 'i' ignora maiusculas e minusculas
+    List<Post> searchTitle(String text);
 }
