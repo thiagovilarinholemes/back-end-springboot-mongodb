d0107944bfe93a92ced6a92b3cf5bcf10d77338e
Thiago V. Lemes <lemes_vilarinho@yahoo.com.br>
2020-06-10 09:21:47 -0300

    Obtendo um usuário por id

--- src/main/java/com/thiagolemes/workshopmongodb/services/UserService.java
+++ src/main/java/com/thiagolemes/workshopmongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.thiagolemes.workshopmongodb.services;
 
 import com.thiagolemes.workshopmongodb.domain.User;
 import com.thiagolemes.workshopmongodb.repositories.UserRepository;
+import com.thiagolemes.workshopmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +17,11 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
 
